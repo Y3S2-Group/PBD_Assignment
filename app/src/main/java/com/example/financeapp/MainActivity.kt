fae97c8924ad2c079c5f17628a322a97f6dad6aa
IT23226814 <vakesh99@gmail.com>
2026-05-23 18:29:51 +0530

    Add Firebase authentication module; implement IAuthRepository and FirebaseAuthRepository

--- app/src/main/java/com/example/financeapp/MainActivity.kt
+++ app/src/main/java/com/example/financeapp/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.financeapp.ui.navigation.MainScreen
+import com.example.financeapp.ui.navigation.AppNavigation
 import com.example.financeapp.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                MainScreen()
+                AppNavigation()
             }
         }
     }
