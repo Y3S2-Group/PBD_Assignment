238fecaadc58410fb8048865ef212092807bc4e2
dinukacodes <dinukajkdy2.0@gmail.com>
2026-05-23 23:46:14 +0530

    Add SavingsDeposit entity to IncomeDatabase and provide SavingsDepositDao in IncomeModule

--- app/src/main/java/com/example/financeapp/data/local/IncomeDatabase.kt
+++ app/src/main/java/com/example/financeapp/data/local/IncomeDatabase.kt
@@ -6,11 +6,17 @@ import com.example.financeapp.domain.model.BudgetCategory
 import com.example.financeapp.domain.model.Expense
 import com.example.financeapp.domain.model.Goal
 import com.example.financeapp.domain.model.Income
+import com.example.financeapp.domain.model.SavingsDeposit
 
-@Database(entities = [Income::class, Expense::class, Goal::class, BudgetCategory::class], version = 4, exportSchema = false)
+@Database(
+    entities = [Income::class, Expense::class, Goal::class, BudgetCategory::class, SavingsDeposit::class],
+    version = 5,
+    exportSchema = false
+)
 abstract class IncomeDatabase : RoomDatabase() {
     abstract fun incomeDao(): IncomeDao
     abstract fun expenseDao(): ExpenseDao
     abstract fun goalDao(): GoalDao
     abstract fun budgetDao(): BudgetDao
+    abstract fun savingsDepositDao(): SavingsDepositDao
 }
